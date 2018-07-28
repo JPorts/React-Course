219e78b034c8c620927515f6e5f15e5fa9222922
Jordan Porter <jordan.1.porter@hotmail.co.uk>
2018-07-28 16:01:24 +0100

    Still reacting away
    Refactored a few files to better adhere to react best practices.

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -3,8 +3,7 @@ import React, { Component } from 'react';
 import classes from './App.css';
 import Person from '../Components/Persons/Person/Person';
 import Persons from '../Components/Persons/Persons'
-import { timingSafeEqual } from 'crypto';
-
+import Cockpit from '../Components/Cockpit/Cockpit'
 // App Class with components within//
 // First React Course // 
 // Jordan Porter 2018 // 
@@ -66,59 +65,25 @@ class App extends Component {
 
   render() {
 
-  
-
     let persons = null;
-    let btnClass = '';
 
-    if(this.state.showPersons){
-      
-      persons = (
-
-        <div>
-          <Persons persons = {this.state.persons}
+    if(this.state.showPersons){    
+      persons =  
+           <Persons persons = {this.state.persons}
             clicked = {this.deletePersonHandler} 
             changed = {this.nameChangedHandler} />
+   }
 
-          {this.state.persons.map((person, index) => {
-
-            return <Person
-              click={() => this.deletePersonHandler(index)}
-              name={person.name}
-              age={person.age}
-              key= {person.id}
-              changed = {(event) => this.nameChangedHandler(event, person.id)}/>
-          })}
-      </div>
-
-      );
-
-      btnClass = classes.Red;
-
-    }
-
-    let assignedClasses =[];
-
-    if(this.state.persons.length <=2) {
-      assignedClasses.push(classes.red); //Classes = ['red']
-    }
-    if(this.state.persons.length <=1){
-      assignedClasses.push(classes.bold); //Classes = ['red','bold']
-    }
-
-
-
-
+  
     return (
       
        <div className={classes.App}>
-
-           <h1>Hi, I'm a React App</h1>
-           <p className={assignedClasses.join(' ')}>This is really working !</p>
-           <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
-
+            <Cockpit
+              appTitle={this.props.title}
+              showPersons={this.state.showPersons}
+              persons={this.state.persons}
+              clicked={this.togglePersonsHandler} />
            {persons}
-
        </div>
 
     );
